1c6523f348cef4e86ec1a1c109bbb277dd71e50d
OriginalSin <serg@auchat.ru>
2024-12-13 22:45:59 +0300

    pr

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,142 @@
 <script>
   	import { onMount } from 'svelte';
 
+    
+    import svetof from './assets/334.jpg'
+    // import svetof from './assets/svetof.png'
+    import './assets/anticon.css'
+  // import Counter from './lib/Counter.svelte'
 
-  import svetof from './assets/svetof.png'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  // import { gamutMapOKLCH, DisplayP3Gamut, sRGBGamut, OKLCH, serialize } from "@texel/color";
+  import * as colors from "@texel/color";
 
-  import { gamutMapOKLCH, DisplayP3Gamut, sRGBGamut, OKLCH, serialize } from "@texel/color";
+  let canva, image, timer, ctx, data, imageData, data1;
+  let delta = $state(0.012);
+  let points = $state([]);
+	let fillings = $state([]);
 
-  let mCanvas;
+  const imageOnLoad = (ev) => {
+    image = ev.target;
+    update();
+    // debugger
+  }
+  const onClick = (ev) => {
+    const {offsetX:x, offsetY:y} = ev;
+    const {width, height} = canva;
 
-  // Some value that may or may not be in sRGB gamut
-  const oklch = [ 0.15, 0.425, 30 ];
+    // const imageData1 = ctx.getImageData(0, 0, width, height);
+    // let data = new Uint8Array(imageData1.data.buffer);
 
-  // decide what gamut you want to map to
-  const isDisplayP3Supported = true; /* check env */;
-  const gamut = isDisplayP3Supported ? DisplayP3Gamut : sRGBGamut;
+    // const {width:w, height:h} = imageData1;
 
-  // map the input OKLCH to the R,G,B space (sRGB or DisplayP3)
-  const rgb = gamutMapOKLCH(oklch, gamut);
+    let pos = width * y + x;
+    let p = 4 * pos;
+    let rgb = [data1[p]/256, data1[p+1]/256, data1[p+2]/256];
+    const lch = colors.convert(rgb, colors.sRGB, colors.OKLab);
+    points.push({lch, x, y, pos});
+    // console.log('points:', points, fillings)
 
-  // get a CSS color string for your output space
-  const color = serialize(rgb, gamut.space);
+    update();
 
+// debugger
+
+  }
 onMount(() => {
-  const t = serialize([1, 0, 0], OKLCH); // "oklch(1 0 0)"
-  console.log('serialize:', t);
-  // draw color to a Canvas2D context
-  // const canvas = document.createElement('canvas');
-  const context = mCanvas.getContext('2d', {
-    colorSpace: gamut.id
-  });
-  context.fillStyle = color;
-  context.fillRect(0,0, mCanvas.width, mCanvas.height);
+  ctx = canva.getContext('2d');
+  canva.addEventListener('mousedown', onClick);
+  // debugger
+  const lch = colors.convert([1, 0, 0], colors.sRGB, colors.OKLab);
+
+  // const t = serialize([1, 0, 0], OKLCH); // "oklch(1 0 0)"
+  console.log('lch:', lch);
+  update();
 });
 
+function update() {
+    cancelAnimationFrame(timer);
+    timer = requestAnimationFrame(function() {
+        render();
+        trace();
+    });
+}
+
+function render() {
+  if (!image) return;
+    // options = dash.getBound();
+    const {width, height} = image;
+    canva.width = width; canva.height = height;
+    // debugger
+    ctx.clearRect(0, 0, width, height);
+    // ctx.setTransform(1, 0, 0, 1, canva.width * 0.5, canva.height * 0.5);
+    // ctx.rotate(options.rotation / 180 * Math.PI);
+    // ctx.scale(1.3, 1.3);
+    ctx.drawImage(image, 0, 0);
+    // ctx.drawImage(img, -img.width * 0.5, -img.height * 0.5);
+    // ctx.setTransform(1, 0, 0, 1, 0, 0);
+    
+    imageData = ctx.getImageData(0, 0, width, height);
+    data = new Uint8Array(imageData.data.buffer);
+  if (!data1) {
+    let imageData1 = ctx.getImageData(0, 0, width, height);
+    data1 = new Uint8Array(imageData1.data.buffer);
+
+    // data1 = data.slice();
+  }
+}
+
+function trace() {
+  if (!image || !points.length) return;
+  const d = delta;
+  let tt = {};
+
+  for(let i = 0; i < data.length; i+=4) {
+    if (data[i+3] === 0) continue;
+    const rgb = [data[i]/256, data[i+1]/256, data[i+2]/256];
+    const lch = colors.convert(rgb, colors.sRGB, colors.OKLab);
+    let out = 100;
+    for(let j = 0; j < points.length; j++) {
+      if (points[j].disable) continue;
+      let r = colors.deltaEOK(lch, points[j].lch);
+      if (r < d) {
+         out = 255;
+         tt[r.toPrecision(3)] = {j, lch}
+         break;
+      }
+    }
+    data[i+3] = out;
+  }
+  // console.log('lch:', tt);
+  ctx.putImageData(imageData, 0, 0);        // synchronous
+
+}
 </script>
 
 <main>
-  <details class="svetof">
-    <summary>svetof</summary>
-    <div>
-      <img src={svetof} class="svetof" alt="svetof" />
-    </div>
-  </details>
-
-  <h1>++++</h1>
 
   <div class="card">
-    <canvas bind:this={mCanvas}></canvas>
+    <canvas bind:this={canva}></canvas>
   </div>
+  <div class="buttons">
+    <input on:change={update} bind:value={delta} class="delta" type="number" step="0.001" min="0" /> - предел
+    <button class="anticon anticon-info" aria-label="info"></button>
+    <button class="anticon anticon-down-circle-o" aria-label="down"></button>
+  </div>
+
+  <details class="svetof" open>
+    <summary>Точки</summary>
+    <div class="card">
+      <img on:load={imageOnLoad} src={svetof} class="svetof" alt="svetof" />
+    </div>
+    <ul>
+      {#each points as item, i}
+        {@const chk = item.disable ? false : true}
+
+        <li><input on:change={(ev)=>{item.disable = !ev.checked; update();}} value="{i}" type="checkbox" checked={chk} /> {JSON.stringify(item)}</li>
+      {/each}
+    </ul>
+
+  </details>
+
 
   <ul>
     <li>
@@ -65,9 +152,27 @@ onMount(() => {
 </main>
 
 <style>
-  main canvas {
+  /* main canvas {
     width: 600px;
     height: 600px;
+  } */
+  .card img {
+    display: none;
+  }
+  .card {
+    border: 1px solid;
+    background-color: blue;
+    width: fit-content;
+  }
+  .delta {
+    width: 4rem;
+  }
+  main ul {
+    text-align: left;
+    width: 22rem;
+  }
+  .buttons button {
+    /* font-family: "ant"; */
   }
   /* .logo {
     height: 6em;
