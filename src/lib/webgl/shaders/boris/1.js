e03bb6740ed86f8a419cb169b246f1edc9e2290d
Boris Alekseev <a402539@yandex.ru>
2025-01-11 14:17:19 +0300

    Update 1.js
    Заполнение тройной матрицы m2

--- src/lib/webgl/shaders/boris/1.js
+++ src/lib/webgl/shaders/boris/1.js
@@ -1,62 +1,15 @@
-const m = [
-    [0, 0, 0, 1, 0, 1],
-    [1, 1, 0, 1, 1, 1],
-    [0, 0, 1, 0, 1, 0],
-    [1, 0, 1, 1, 1, 1]
-];
+const m = Array(
+    Array(0, 0, 0, 1, 0, 1),
+    Array(1, 1, 0, 1, 1, 1),
+    Array(0, 0, 1, 0, 1, 0),
+    Array(1, 0, 1, 1, 1, 1)
+);
 const mr = m.length;
 const mc = m[0].length;
-console.log('init', mr, mc, m);
 
-
-const getD_for = (m) => {
-    const d = [];
-    const dc = 3 * m[0].length;
-
-    let pr;
-    for (let r = 0; r < m.length; r++) {
-        pr = 2 * r;
-        d[pr] = new Array(dc).fill('_');
-        const d1 = new Array(dc).fill('_');
-        const m1 = m[r];
-        for (let c = 0; c < m1.length; c++) {
-            let pc = 2 * c;
-            const v = m1[c];
-
-            d1[1 + pc] = v;
-            // console.log('___', pr, pc, v, d1[pc]);
-        }
-        d[1 + pr] = d1;
-        // console.log('___', pr, d1);
-
+let m2 = Array(3*mr).fill(new Array(3*mc));
+for (let i=0; i<mr; i++){
+    for (let j=0; j<mc; j++){
+        m2[3*i+1][3*j+1] = m[i][j];
     }
-    d[2 + pr] = new Array(dc).fill('_');
-
-    return d;
 }
-
-const getD = (m, fill='') => {
-    const d = [];
-    const dc = 2 * m[0].length + 1;
-
-    let pr;
-    m.forEach((m1, r)=>{
-        pr = 2 * r;
-        d[pr] = new Array(dc).fill(fill);
-        const d1 = new Array(dc).fill(fill);
-        m1.forEach((v, c)=>{
-            let pc = 2 * c;
-            d1[1 + pc] = v;
-        });
-        d[1 + pr] = d1;
-    
-    })
-    d[2 + pr] = new Array(dc).fill(fill);
-    return d;
-}
-
-// console.log('res', getD_for(m));
-const d = getD(m, '');
-console.log('res', d.length, d[0].length, d);
-
-// node F:\gitGlobe\nColor\src\lib\webgl\shaders\boris\1.js
